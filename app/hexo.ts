2c8d5e8efcd0414b45ff06504c84efe6769b46c2
confuseder <g200803092018@163.com>
2025-03-29 05:43:32 +0000

    Update Hexo Tool

--- app/hexo.ts
+++ app/hexo.ts
@@ -10,16 +10,13 @@ export const initHexo = cache(async (): Promise<Hexo> => {
   }
 
   const hexo = new Hexo(join(process.cwd(), 'Hexo'), {
-    silent: true,
-    drafts: process.env.NODE_ENV !== "production",
+    silent: true
   });
 
   await hexo.init();
   await hexo.load();
-  // 在生产环境中，将 Hexo 的 warehouse 中的数据写入文件系统
-  if (hexo.env.init && hexo._dbLoaded) {
-    await hexo.database.save();
-  }
+  await hexo.database.save();
+
   __SECRET_HEXO_INSTANCE__ = hexo;
   return hexo;
 });
